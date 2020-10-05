974bdac24097f3460b3dd7d7985d5269b4d768fd
vaibhavs <vaibhavs@sfu.ca>
2020-10-04 18:26:43 -0700

    created list_lens

--- app/src/main/java/com/vaibhavs/depthoffieldcalculator/UI/MainActivity.java
+++ app/src/main/java/com/vaibhavs/depthoffieldcalculator/UI/MainActivity.java
@@ -4,29 +4,65 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vaibhavs.depthoffieldcalculator.Model.Lens;
 import com.vaibhavs.depthoffieldcalculator.Model.LensManager;
 import com.vaibhavs.depthoffieldcalculator.R;
 
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    LensManager lenses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         LensManager lenses = LensManager.getInstance();
         add_defaultlenses(lenses);
+        pop_Listview();
         add_morelensbutton();
+        getLensIndex();
         }
+
+
+    private void add_defaultlenses(LensManager manager) {
+
+        manager.add(new Lens("Canon", 1.8, 50));
+        manager.add(new Lens("Tamron", 2.8, 90));
+        manager.add(new Lens("Sigma", 2.8, 200));
+        manager.add(new Lens("Nikon", 4.0, 200));
+    }
+
+    private void pop_Listview() {
+        ArrayAdapter<LensManager> adapter = new ArrayAdapter<LensManager>(this,R.layout.list_lens, Collections.singletonList(lenses));
+        ListView list = findViewById(R.id.Lens_list);
+        list.setAdapter(adapter);
+    }
+
+    private void getLensIndex() {
+        ListView list = findViewById(R.id.Lens_list);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView lens_selected = (TextView) view;
+                String message = "You have selected " + lens_selected.getText().toString();
+                Toast.makeText(MainActivity.this,message,Toast.LENGTH_LONG);
+
+            }
+        });
+    }
 
     private void add_morelensbutton() {
         FloatingActionButton fab = findViewById(R.id.Add_lens);
@@ -37,12 +73,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-    private void add_defaultlenses(LensManager manager) {
-
-        manager.add(new Lens("Canon", 1.8, 50));
-        manager.add(new Lens("Tamron", 2.8, 90));
-        manager.add(new Lens("Sigma", 2.8, 200));
-        manager.add(new Lens("Nikon", 4.0, 200));
     }
 }
